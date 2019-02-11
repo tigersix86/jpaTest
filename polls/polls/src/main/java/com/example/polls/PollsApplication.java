3a55959862d1c0c94e3f2058ab29467d3466fb4d
Magnus Nordin <magnus.nordin@kvartsgatan13.se>
2019-02-11 20:18:07 +0100

    added code

--- polls/polls/src/main/java/com/example/polls/PollsApplication.java
+++ polls/polls/src/main/java/com/example/polls/PollsApplication.java
@@ -43,8 +43,8 @@ public class PollsApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-//		roleRepo.save(new Role(RoleName.ROLE_ADMIN));
-//		roleRepo.save(new Role(RoleName.ROLE_USER));
+		roleRepo.save(new Role(RoleName.ROLE_ADMIN));
+		roleRepo.save(new Role(RoleName.ROLE_USER));
 		User user = new User("Magnus","magnus","m@kvartsgatan13.se","pass");
 		Set<Role> roles = new HashSet<>();
 		Optional<Role> role = roleRepo.findByName(RoleName.ROLE_ADMIN);
